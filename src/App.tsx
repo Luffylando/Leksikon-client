076a5a61deac72e482efc0d6a177120d8b86e8ab
OgistDipen <ogistdipen@outlook.com>
2023-06-16 21:56:29 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import User from './Pages/Users';
+import Question from './Pages/Questions';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/users" element={<User />} />
+      <Route path="/questions" element={<Question />} />
+    </Routes>
   );
 }
 
